02e7e8b1501590fdae3a9d6240786e482295c1fd
Wewi <joaoaizpuru@gmail.com>
2022-10-28 17:55:33 -0300

    trabalhando com numeros e testos

---
+++ testos_numeros.py
@@ -0,0 +1,16 @@
+from cmath import sqrt
+
+
+import math
+nome_cachorro = "bartolomeo"
+message = 'Python Is Fun'
+cuadrado = 16
+
+if nome_cachorro == "bartolomeo":
+    print ("meu novo cachorro")
+else:
+    print("meu antigo cachorro")
+
+# diminuindo a letra 
+print (message.lower())
+print (math.sqrt(cuadrado))
